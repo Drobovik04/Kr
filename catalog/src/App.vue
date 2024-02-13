fdc81cc7c5b3088114e6d853c0085782eb4a565c
Drobovik04 <misle92@mail.ru>
2024-02-13 21:58:58 +0300

    набросок стартовой страницы

---
+++ catalog/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+<sidebar>
+    <div class="filters">
+        <div class="headfilter">
+            Фильтрация
+        </div>
+        <input type="text" id="filterinput" placeholder="Название">
+        <div class="tags_container">
+
+        </div>
+    </div>
+</sidebar>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    #app
+    {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        height: 500px;
+        width: 350px;
+        position: relative;
+        left: 0px;
+    }
+    sidebar
+    {
+        display: flex;
+        flex-direction: column;
+        height: 500px;
+        width: 350px;
+        position: relative;
+        top: 10%;
+        left: 0px;
+        background-color: rgb(224, 224, 224);
+        border-radius: 20px;
+    }
+    .headfilter
+    {
+        display: flex;
+        justify-content: center;
+    }
+    #filterinput
+    {
+        width: 100%;
+
+    }
+</style>
